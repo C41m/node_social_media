d0f0136c97e137f1742ec2102cb8251e2e4e51ff
C41m <caiofernandobs@gmail.com>
2024-01-24 11:57:08 -0300

    Adicionar .gitignore

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "./login.scss"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis incidunt a sunt! Error quos quisquam corrupti minima, deserunt quaerat unde. Pariatur et voluptatibus porro. Debitis asperiores voluptatibus velit culpa magnam.</p>
+                    <span>Crie sua conta!</span>
+                    <button>Cadastre-se</button>
+                </div>
+                <div className="right"></div>
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Usuário" />
+                    <input type="password" placeholder="Senha"/>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login
