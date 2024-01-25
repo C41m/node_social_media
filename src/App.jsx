cd65068beed56a39f638b1f90f29e0fb13963a4f
C41m <caiofernandobs@gmail.com>
2024-01-25 10:12:38 -0300

    Configuraçao Inicial de Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,52 @@ import Login from "./pages/login/Login"
 import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
-
+import RightBar from "./components/rightBar/RightBar";
+import LeftBar from "./components/leftBar/leftBar";
+import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile"
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
   const router = createBrowserRouter([
     {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ],
+    },
+    {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
-    
   ]);
 
   return (
