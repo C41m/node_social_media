c5178dd3c6b5d8c2c4cba3f12879ae55cc49398f
C41m <caiofernandobs@gmail.com>
2024-01-26 12:17:31 -0300

    Desenvolvimento da NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,9 +1,42 @@
-import "./navbar.scss"
+import './navbar.scss';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">NavBar</div>
-  )
-}
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <span>CaioSocial</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Procurar..." />
+        </div>
+      </div>
+      <div className="right">
+        <AccountCircleOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsNoneOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,29 +1,34 @@
-import { Link } from "react-router-dom"
-import "./login.scss"
+import { Link } from 'react-router-dom';
+import './login.scss';
 
 const Login = () => {
-    return (
-        <div className="login">
-            <div className="card">
-                <div className="left">
-                    <h1>Hello World</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis incidunt a sunt! Error quos quisquam corrupti minima, deserunt quaerat unde. Pariatur et voluptatibus porro. Debitis asperiores voluptatibus velit culpa magnam.</p>
-                    <span>Crie sua conta!</span>
-                    <Link to='/register'>
-                        <button>Cadastre-se</button>
-                    </Link>
-                </div>
-                <div className="right">
-                    <h1>Login</h1>
-                    <form>
-                        <input type="text" placeholder="Usuário" />
-                        <input type="password" placeholder="Senha" />
-                        <button>Login</button>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
+            incidunt a sunt! Error quos quisquam corrupti minima, deserunt
+            quaerat unde. Pariatur et voluptatibus porro. Debitis asperiores
+            voluptatibus velit culpa magnam.
+          </p>
+          <span>Crie sua conta!</span>
+          <Link to="/register">
+            <button>Cadastre-se</button>
+          </Link>
         </div>
-    )
-}
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Usuário" />
+            <input type="password" placeholder="Senha" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
