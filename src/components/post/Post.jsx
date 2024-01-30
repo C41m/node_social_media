78244bfbea77d6979960cea9b58884ca6abe784a
C41m <caiofernandobs@gmail.com>
2024-01-30 17:20:13 -0300

    Visual dos Posts

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,57 @@
+import './post.scss';
+import { Link } from 'react-router-dom';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ShareIcon from '@mui/icons-material/Share';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+
+const Post = ({ post }) => {
+  const liked = false;
+
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="" />
+            <div className="details">
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date">1 min atrás</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={post.img} alt="" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {liked ? (
+              <FavoriteOutlinedIcon className="icon" />
+            ) : (
+              <FavoriteBorderOutlinedIcon className="icon" />
+            )}
+            <span>12 Gostei</span>
+          </div>
+          <div className="item">
+            <ChatOutlinedIcon className="icon" />
+            <span>12 Comentários</span>
+          </div>
+          <div className="item">
+            <ShareIcon className="icon" />
+            <span>12 Compartilhamentos</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,7 +1,54 @@
-import "./posts.scss"
+import './posts.scss';
+import Post from '../post/Post';
 
 const Posts = () => {
-  return <div className="Posts">Posts</div>;
+  //
+  const posts = [
+    {
+      id: 1,
+      name: 'John Doe',
+      userId: 1,
+      profilePic:
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, qui.',
+      img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      id: 2,
+      name: 'John Doe',
+      userId: 2,
+      profilePic:
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, qui.',
+      img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      id: 3,
+      name: 'John Doe',
+      userId: 3,
+      profilePic:
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, qui.',
+      img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      id: 4,
+      name: 'John Doe',
+      userId: 4,
+      profilePic:
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, qui.',
+      img: 'https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ];
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
